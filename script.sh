9dd4f3cc5aceca2f8df273ddedf5432262430dfc
itzatom <vittorio.fones@gmail.com>
2018-05-21 12:36:47 +0200

    bugfixed script.sh (not adding .vim folder correctly in home)

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ VUNDLEGIT="https://github.com/VundleVim/Vundle.vim.git"
 
 mv $HOME/.vim{,bk} && mv $HOME/.vimrc{,bk}
 
-mv dotfiles/.vim* $HOME/
+mv dotfiles/vim/.vim* $HOME/
 
 git clone $VUNDLEGIT $HOME/.vim/bundle/Vundle.vim
 
