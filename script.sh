0f7c356d7432d8c369c84ba6577fd1176030ef78
Vittorio Fones <vittorio.fones@gmail.com>
2018-11-11 19:30:32 +0100

    Automating restoring and backup

--- script.sh
+++ script.sh
@@ -1,10 +1,45 @@
 #!/bin/sh
 
 VUNDLEGIT="https://github.com/VundleVim/Vundle.vim.git"
+OHMYZSHGIT="https://github.com/robbyrussell/oh-my-zsh"
 
-mv $HOME/.vim{,bk} && mv $HOME/.vimrc{,bk}
+BACKUPFOLDER=($PWD/backup)
+DOTCONFIG=($PWD/dotfiles/dotconfig)
+DOTHOME=($PWD/dotfiles/dothome)
 
-mv dotfiles/vim/.vim* $HOME/
+mkdir $BACKUPFOLDER
+git clone $OHMYZSHGIT $DOTHOME/oh-my-zsh/.oh-my-zsh/
+
+
+#Backing up file in .config folder
+cd $DOTCONFIG
+for dotfile in $(ls);
+do
+    tmp="$HOME/.config/$dotfile"
+    if [ -e "$tmp" ]; then
+        mv $tmp $BACKUPFOLDER
+    fi
+done
+#Creating symlink
+for dotfile in $(ls)
+do
+   stow -v -t $HOME -R $dotfile
+done
+
+#Backing up dotfiles in home
+cd $DOTHOME
+for dotfile in $(ls);
+do
+    tmp="$HOME/.$dotfile"
+    if [ -e "$tmp" ]; then
+        mv $tmp $BACKUPFOLDER
+    fi
+done
+#Creating symlink
+for dotfile in $(ls)
+do
+   stow -v -t $HOME -R $dotfile
+done
 
 git clone $VUNDLEGIT $HOME/.vim/bundle/Vundle.vim
 
