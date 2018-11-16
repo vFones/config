5262743147f16f7b6347ae660e877547c4c037c9
Vittorio Fones <vittorio.fones@gmail.com>
2018-11-16 18:17:07 +0100

    modified config (removed mpd and bumblestatus, addedd fehbg and polybar launcher). Addedd redshift values in bar and modified margin

--- dotfiles/dotconfig/polybar/.config/polybar/redshift.sh
+++ dotfiles/dotconfig/polybar/.config/polybar/redshift.sh
@@ -5,9 +5,9 @@ icon=""
 temp=$(redshift -p 2>/dev/null| grep temp | awk '{ print $3 }' | cut -c -4)
 
 if [[ $temp -ge 5000 ]]; then
-        echo "%{F#458588}$icon"       # Blue
+        echo "%{F#458588}$icon $temp"       # Blue
 elif [[ $temp -ge 4000 ]]; then
-        echo "%{F#d79921}$icon"       # Yellow
+        echo "%{F#d79921}$icon $temp"       # Yellow
 else
-        echo "%{F#cc241d}$icon"       # Orange
+        echo "%{F#cc241d}$icon $temp"       # Orange
 fi
