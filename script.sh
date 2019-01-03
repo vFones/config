a68306e1c6eeb49842cb9c4af1aa71b3d8b106e7
itzatom <vittorio.fones@gmail.com>
2019-01-03 15:42:40 +0100

    Added alias, starting refactoring in script.sh

--- script.sh
+++ script.sh
@@ -2,40 +2,53 @@
 
 #install zsh, gawk, fzy, vim AT LEAST
 
-BACKUPFOLDER=$PWD/backup
-DOTCONFIG=$PWD/dotfiles/dotconfig
-DOTHOME=$PWD/dotfiles/dothome
+lnstuff(){
+  for dotfile in $(ls)
+  do
+    stow -v -t $HOME -R $dotfile
+  done
+}
 
-mkdir $BACKUPFOLDER
-
-#Backing up file in .config folder
-cd $DOTCONFIG
-for dotfile in $(ls);
-do
+backuper(){
+  cd $1
+  for dotfile in $(ls);
+  do
     tmp="$HOME/.config/$dotfile"
     if [ -e "$tmp" ]; then
         mv $tmp $BACKUPFOLDER
     fi
-done
-#Creating symlink
-for dotfile in $(ls)
-do
-   stow -v -t $HOME -R $dotfile
-done
+  done
+}
 
-#Backing up dotfiles in home
-cd $DOTHOME
-for dotfile in $(ls);
-do
-    tmp="$HOME/.$dotfile"
-    if [ -e "$tmp" ]; then
-        mv $tmp $BACKUPFOLDER
-    fi
-done
-#Creating symlink
-for dotfile in $(ls)
-do
-   stow -v -t $HOME -R $dotfile
-done
+BACKUPFOLDER=$PWD/backup
+DOTCONFIG=$PWD/dotfiles/dotconfig
+DOTHOME=$PWD/dotfiles/dothome
 
-vim +PlugInstall +qall
+if [ -d -e $BACKUPFOLDER ]; then
+  mkdir $BACKUPFOLDER
+fi
+
+if test "$(uname)" = "Darwin"; then
+  if test ! $(which gcc); then
+    echo "Installing xcode..."
+    xcode-select --install
+  fi
+  if test ! "$(which brew)"; then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    brew update
+  fi
+fi
+
+backuper $DOTCONFIG
+lnstuff
+backuper $DOTHOME
+lnstuff
+
+brew install coreutil zplug
+pip install powerline-shell
+
+vim +'PlugInstall --sync' +qa
+vim +'PlugUpdate' +qa
+
