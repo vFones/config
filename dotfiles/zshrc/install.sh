6a10584e98db7558977eee1c4b0a60aef8dbc24c
itzatom <vittorio.fones@gmail.com>
2019-01-04 00:32:44 +0100

    fixed .zsh_plugins name

--- dotfiles/zshrc/install.sh
+++ dotfiles/zshrc/install.sh
@@ -4,6 +4,6 @@ echo "Linking zsh stuff to home..."
 ln -s -f $(pwd)/zshrc.link $HOME/.zshrc
 ln -s -f $(pwd)/zsh_plugins.link $HOME/.zsh_plugins
 
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+antibody bundle < ~/.zsh_plugins > ~/.zsh_plugins.sh
 echo "set zsh shell: chsh -s $(which zsh)"
 echo "Restart terminal or zsh to apply"
