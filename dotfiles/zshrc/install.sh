9b19018db6942d1bd671113c2667ee1bdb769ae7
Vittorio Fones <vittorio.fones@gmail.com>
2019-03-31 22:21:54 +0200

    No more using powerlevel9k. Clean af

--- dotfiles/zshrc/install.sh
+++ dotfiles/zshrc/install.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 echo "Linking zsh stuff to home..."
-ln -s -f $1/zshrc/zshrc.link $HOME/.zshrc
-ln -s -f $1/zshrc/zsh_plugins.link $HOME/.zsh_plugins
-
+ln -s -f -v $PWD/dotfiles/zshrc/zshrc.link $HOME/.zshrc
+ln -s -f -v $PWD/dotfiles/zshrc/zsh_plugins.link $HOME/.zsh_plugins
 antibody bundle < ~/.zsh_plugins > ~/.zsh_plugins.sh
-echo "set zsh shell: chsh -s $(which zsh)"
-echo "Restart terminal or zsh to apply"
+
+chsh -s $(which zsh)
+
+echo "Reboot/Log out to see changes"
