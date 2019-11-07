d928ff990b5fd1523533a3128e0a767bda7784c2
Vittorio Fones <vittorio.fones@gmail.com>
2019-11-07 20:21:18 +0100

    Small bug fixes

--- dotfiles/zshrc/install.sh
+++ dotfiles/zshrc/install.sh
@@ -5,6 +5,6 @@ ln -s -f -v $PWD/dotfiles/zshrc/zshrc.link $HOME/.zshrc
 ln -s -f -v $PWD/dotfiles/zshrc/zsh_plugins.link $HOME/.zsh_plugins
 antibody bundle < ~/.zsh_plugins > ~/.zsh_plugins.sh
 
-chsh -s $(which zsh)
+chsh -s /bin/zsh
 
 echo "Reboot/Log out to see changes"
