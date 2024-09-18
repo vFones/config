204d75909734ae08e7ca7ed6e4b395bed550c7b4
Vittorio Fones <vittorio.fones@activadigital.it>
2024-09-18 11:04:29 +0200

    feat(linux): modified config for i3 and polybar

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -10,6 +10,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
     MONITOR=$m polybar -r -q main -c "$DIR"/config.ini &
